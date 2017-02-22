b622b4dcda9a9ed8fbf2ab65b0bd549f05950aed
Paige Brinks <plb7@calvin.edu>
2017-02-22 10:37:32 -0500

    completed exercise 3.3

---
+++ 03modeling/homework03_3.sql
@@ -0,0 +1,10 @@
+-- Paige Brinks
+-- cs 342
+-- Homework 03_3
+-- 02/22/2017
+
+-- Consider the ER design for the MAIL_ORDER database that was modeled
+-- using a tool like ERwin or Rational Rose in Laboratory Exercise 3.32. Using
+-- the SQL schema generation feature of the modeling tool, generate the SQL
+-- schema for an Oracle database.
+
--- 03modeling/lab03.sql
+++ 03modeling/lab03.sql
@@ -7,8 +7,8 @@
 -- See cpdb-partial.pdf for ERD
 
 
+drop table Request;
 drop table PersonTeam;
-drop table PersonHousehold;
 drop table Team;
 drop table Person;
 drop table HouseHold;
@@ -48,20 +48,13 @@ create table Person (
 	firstName varchar(15),
 	lastName varchar(15),
 	membershipStatus char(1) CHECK (membershipStatus IN ('m', 'a', 'c')),
+	householdID integer,
 	mentorID integer,
 	homegroupID integer,
+	role varchar(20) CHECK (role IN ('parent', 'child', 'single', 'student')),
+	FOREIGN KEY (houseHoldID) REFERENCES HouseHold(ID) ON DELETE SET NULL,
 	FOREIGN KEY (mentorID) REFERENCES Person(ID) ON DELETE SET NULL,
 	FOREIGN KEY (homegroupID) REFERENCES Homegroup(ID) ON DELETE SET NULL
-	);
-
-	
-create table PersonHousehold (
-	personID integer,
-	houseHoldID integer,
-	role varchar(20) CHECK (role IN ('parent', 'child', 'single', 'student')),
-	PRIMARY KEY (personID, houseHoldID),
-	FOREIGN KEY (personID) REFERENCES Person(ID) ON DELETE CASCADE,
-	FOREIGN KEY (householdID) REFERENCES HouseHold(ID) ON DELETE CASCADE
 	);
 
 create table PersonTeam (
@@ -73,6 +66,17 @@ create table PersonTeam (
 	FOREIGN KEY (teamID) REFERENCES Team(ID) ON DELETE SET NULL
 	);
 
+create table Request (
+	requestor integer,
+	responder integer,
+	submitDate date,
+	text varchar(100),
+	accessCode char,
+	response varchar(100),
+	PRIMARY KEY (requestor, responder, submitDate),
+	FOREIGN KEY (requestor) REFERENCES HouseHold(ID) ON DELETE SET NULL,
+	FOREIGN KEY (responder) REFERENCES Person(ID) ON DELETE SET NULL
+	);
 
 -- fill tables with sample data
 INSERT INTO Homegroup VALUES (0, 'Womens Bible Study', 'Chapel', '15:00', 'Monday', 'Women get together to read the book of Ruth');
@@ -88,22 +92,18 @@ INSERT INTO Household VALUES (1,'123 Burton','Grand Rapids','MI','49546','616-12
 INSERT INTO Household VALUES (2,'456 Woodcliffe','Grand Rapids','MI','49546','616-548-1548');
 
 
-INSERT INTO Person VALUES (0,'mr.','Keith','VanderLinden','m', NULL, 2);
-INSERT INTO Person VALUES (1,'ms.','Brenda','VanderLinden','m', NULL, 0);
-INSERT INTO Person VALUES (2,'ms.','Paige','Brinks','m', 2, NULL);
-INSERT INTO Person VALUES (3,'ms.','Lydia','Cupery','m', 2, 1);
-INSERT INTO Person VALUES (4,'ms.','Beka','Agava','m', 3, 1);
-
-
-INSERT INTO PersonHousehold VALUES (0, 0, 'parent');
-INSERT INTO PersonHousehold VALUES (1, 0, 'parent');
-INSERT INTO PersonHousehold VALUES (2, 1, 'student');
-INSERT INTO PersonHousehold VALUES (3, 2, 'single');
-INSERT INTO PersonHousehold VALUES (4, 1, 'student');
+INSERT INTO Person VALUES (0,'mr.','Keith','VanderLinden','m', 0, NULL, 2, 'parent');
+INSERT INTO Person VALUES (1,'ms.','Brenda','VanderLinden','m', 0, NULL, 0, 'parent');
+INSERT INTO Person VALUES (2,'ms.','Paige','Brinks','m', 2, 1, NULL, 'student');
+INSERT INTO Person VALUES (3,'ms.','Lydia','Cupery','m', 2, 2, 1, 'single');
+INSERT INTO Person VALUES (4,'ms.','Beka','Agava','m', 1, 3, 1, 'student');
 
 INSERT INTO PersonTeam VALUES (0, 1, 'secretary');
 INSERT INTO PersonTeam VALUES (1, 0, 'member');
 INSERT INTO PersonTeam VALUES (2, 0, 'treasurer');
 INSERT INTO PersonTeam VALUES (3, 0, 'member');
+
+INSERT INTO Request VALUES (0, 3, '01-FEB-2017', 'My dog is sick.', 'a', 'I will pray for your dog');
+INSERT INTO Request VALUES (1, 1, '15-FEB-2017', 'Im sick', 'b', NULL);
 
 
