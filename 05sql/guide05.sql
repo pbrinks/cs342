524a82477bc0afa6091e4d69e5290c5d89304646
Paige Brinks <Paige Brinks>
2017-02-27 10:41:47 -0500

    guide 05

---
+++ 05sql/guide05.sql
@@ -0,0 +1,81 @@
+-- Sample version of the Movies database for guide 5
+--
+-- CS 342, Spring, 2017
+-- kvlinden
+
+-- Drop current database
+DROP TABLE Casting;
+DROP TABLE Movie;
+DROP TABLE Performer;
+
+-- Create database schema
+CREATE TABLE Movie (
+	id integer,
+	title varchar(70) NOT NULL,
+	year decimal(4),
+	score float,
+	votes integer,
+	PRIMARY KEY (id),
+	CHECK (year > 1900)
+	);
+
+CREATE TABLE Performer (
+	id integer,
+	firstName varchar(20),
+	lastName varchar(25),
+	PRIMARY KEY (id)
+	);
+
+CREATE TABLE Casting (
+	movieId integer,
+	performerId integer,
+	status varchar(6),
+	FOREIGN KEY (movieId) REFERENCES Movie(Id) ON DELETE CASCADE,
+	FOREIGN KEY (performerId) REFERENCES Performer(Id) ON DELETE SET NULL,
+	CHECK (status in ('star', 'costar', 'extra'))
+	);
+
+-- Load sample data
+INSERT INTO Movie VALUES (1,'Star Wars',1977,8.9, 2000);
+INSERT INTO Movie VALUES (2,'Blade Runner',1982,8.6, 1500);
+
+INSERT INTO Performer VALUES (1,'Harrison', 'Ford');
+INSERT INTO Performer VALUES (2,'Rutger', 'Hauer');
+INSERT INTO Performer VALUES (3,'Chewbacca', NULL);
+INSERT INTO Performer VALUES (4,'Rachael', NULL);
+INSERT INTO Performer VALUES (5,'Rex', 'Harrison');
+
+INSERT INTO Casting VALUES (1,1,'star');
+INSERT INTO Casting VALUES (1,3,'extra');
+INSERT INTO Casting VALUES (2,1,'star');
+INSERT INTO Casting VALUES (2,2,'costar');
+INSERT INTO Casting VALUES (2,4,'costar');
+
+
+-- guide 5
+-- 1.
+-- a. tuple variable 
+select * from Casting AS c
+where c.status = 'costar';
+
+-- b. set operation
+(select lastName from Performer)
+EXCEPT
+(select * from Performer
+where lastName IS NULL);
+
+-- 2.
+-- a. select based on NULL field value
+select id from Performer
+where lastName IS NULL;
+
+-- b. nested subquery
+select lastName, firstName from Performer
+where id IN (select distinct performerId
+	from Casting);
+	
+-- c. correlated subquery
+select movieId from Casting c1
+where (select movieId from Casting c2
+	where c1.performerId = c2.performerId
+		and c1.status <> c2.status);
