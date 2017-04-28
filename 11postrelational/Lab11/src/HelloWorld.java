c42c92a13eb284085e74736cc912586e8949c160
Paige Brinks <plb7@calvin.edu>
2017-04-27 21:57:02 -0400

    homework 11

--- 11postrelational/Lab11/src/HelloWorld.java
+++ 11postrelational/Lab11/src/HelloWorld.java
@@ -1,9 +1,11 @@
 /**
  * Created by plb7 on 4/21/2017.
  */
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import static java.lang.System.exit;
+
 
 // The Java class will be hosted at the URI path "/helloworld"
 @Path("/hello")
@@ -16,4 +18,39 @@ public class HelloWorld {
         // Return some cliched textual content
         return "Hello World";
     }
+
+    @Path("/api")
+    @GET
+    @Produces("text/plain")
+    public String getMessage() {
+        return "Getting...";
+    }
+
+    @Path("/api/{number}")
+    @PUT
+    @Produces("text/plain")
+    @Consumes("text/plain")
+    public String putInteger(@PathParam("number") Integer number ) {
+        return "Putting: " + number;
+    }
+
+
+    @Path("/api/{message}")
+    @POST
+    @Produces("text/plain")
+    public String postMessage(@PathParam("message") String message ) {
+        return "Posting: " + message;
+    }
+
+
+    @Path("/api/{number}")
+    @DELETE
+    @Consumes("text/plain")
+    public String deleteInteger(@PathParam("number") Integer number ) {
+        if (number < 0 || number > 9) {
+            return "Integer is not between 0 and 9!";
+        }
+        return "Deleting: " + number;
+    }
+
 }
